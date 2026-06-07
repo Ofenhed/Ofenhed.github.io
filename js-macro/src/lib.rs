a6ed3722a97a689f5fd82d1745b722f65c93da26
Marcus <>
2026-06-07 17:08:02 +0200

    fix: minified javascript parsed incorrectly

--- js-macro/src/lib.rs
+++ js-macro/src/lib.rs
@@ -1,10 +1,30 @@
 extern crate proc_macro;
-use proc_macro::TokenStream;
-use std::sync::Arc;
+use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
+use std::{
+    iter::{Extend as _, once},
+    sync::Arc,
+};
+
+fn err_tokens(err: impl std::fmt::Display) -> TokenStream {
+    let mut tokens = TokenStream::default();
+    tokens.extend(once(Ident::new("compile_error", Span::call_site())));
+    tokens.extend(once(Punct::new('!', Spacing::Joint)));
+    let mut inner = TokenStream::default();
+    inner.extend(once(Literal::string(&err.to_string())));
+    tokens.extend(once(Group::new(Delimiter::Parenthesis, inner)));
+    tokens
+}
 
 #[proc_macro]
 pub fn minify_js(input: TokenStream) -> TokenStream {
-    let js = format!("{}", input);
+    let js: String = input
+        .into_iter()
+        .map(|tree| {
+            tree.span()
+                .source_text()
+                .unwrap_or_else(move || tree.to_string())
+        })
+        .collect();
     let cm = Arc::<swc_common::SourceMap>::default();
 
     let c = swc::Compiler::new(cm.clone());
@@ -25,8 +45,8 @@ pub fn minify_js(input: TokenStream) -> TokenStream {
             )
         })
     });
-    proc_macro::TokenTree::Literal(proc_macro::Literal::string(
-        &output.expect("Always minifies").code,
-    ))
-    .into()
+    match output {
+        Ok(output) => TokenTree::Literal(Literal::string(&output.code)).into(),
+        Err(err) => err_tokens(err.to_pretty_string()),
+    }
 }
