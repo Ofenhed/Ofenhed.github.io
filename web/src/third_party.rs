e5607040064c5852ae01bd2bff24c5ce40beefc5
Marcus <>
2026-06-08 13:30:17 +0200

    Add third party elements
    This may be used to query for cookie use and embedding videos

---
+++ web/src/third_party.rs
@@ -0,0 +1,14 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn YouTube(
+    #[prop(into)] id: Signal<String>,
+    #[prop(optional)] children: Option<Children>,
+) -> impl IntoView {
+    view! {
+        //<iframe src=move || id.with(|id| format!("https://www.youtube-nocookie.com/embed/{id}")) sandbox="allow-scripts" allow="fullscreen; encrypted-media"></iframe>
+        <a href=move || id.with(|id| format!("https://youtube.com/watch?v={id}"))>
+        { children.map(|x| x()) }
+        </a>
+    }
+}
