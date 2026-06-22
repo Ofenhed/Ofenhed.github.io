893a94e429a8476b9e8479696b1985b4e4b854d6
Marcus <>
2026-06-22 10:36:39 +0200

    Add some very small introduction (mostly placeholder)

---
+++ web/src/blog/why.rs
@@ -0,0 +1,53 @@
+use crate::blog::metadata::{BlogEntry, Locale, Tag};
+use chrono::{DateTime, Utc};
+use leptos::prelude::*;
+use leptos_router::{LazyRoute, lazy_route};
+
+#[derive(Clone, Copy)]
+pub(crate) struct WhyBlog;
+
+impl BlogEntry for WhyBlog {
+    fn uid() -> u32 {
+        1
+    }
+
+    fn publish_date() -> DateTime<Utc> {
+        DateTime::parse_from_rfc3339("2026-06-22T12:00:00+01:00")
+            .unwrap()
+            .into()
+    }
+
+    fn publish() -> bool {
+        true
+    }
+
+    fn locale() -> Option<Locale> {
+        Locale::EnglishSimplified.into()
+    }
+
+    fn title() -> &'static str {
+        "Another voice on the internet"
+    }
+
+    fn tags() -> &'static [Tag] {
+        &[]
+    }
+
+    fn pin() -> Option<usize> {
+        Some(1)
+    }
+}
+
+#[lazy_route]
+impl LazyRoute for WhyBlog {
+    fn data() -> Self {
+        Self
+    }
+
+    fn view(_this: Self) -> AnyView {
+        let llm = || view! { <abbr title="Large Language Model">LLM</abbr> }.into_inner();
+        view! {
+            "This is an experiment with this new concept I thought of, where someone could share their thought in some kind of captain's log on the web, like web captain's log, or a captain's web log. With all "{llm}" generated text online, we clearly need more people sending text into the dark void that is the modern internet."
+        }.into_any()
+    }
+}
