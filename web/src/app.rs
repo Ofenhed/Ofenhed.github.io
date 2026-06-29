9bb9a2c1feb010288373b4249c19cb5ec1e66ba1
Marcus <>
2026-06-29 21:55:53 +0200

    Why would the content of a <time> be a phone number? Apple are so incredibly shit.

--- web/src/app.rs
+++ web/src/app.rs
@@ -23,6 +23,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="referrer" content="no-referrer" />
+                <meta name="format-detection" content="telephone=no" />
                 <AutoReload options=options.clone() />
                 <HydrationScripts options />
                 <Stylesheet href=css_path />
