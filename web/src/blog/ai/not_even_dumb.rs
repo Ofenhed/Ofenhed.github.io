e6161bd6146e95e98291ce8f469c5c3b51c3468b
Marcus <>
2026-07-17 11:52:36 +0200

    improve collection of AI videos

--- web/src/blog/ai/not_even_dumb.rs
+++ web/src/blog/ai/not_even_dumb.rs
@@ -1,5 +1,6 @@
 use crate::{
     blog::metadata::{BlogEntry, Locale, Tag, date},
+    helpers::Abbr,
     third_party::{YouTube, youtube},
 };
 use chrono::{DateTime, Utc};
@@ -17,7 +18,7 @@ impl BlogEntry for NotEvenDumb {
 
     const LOCALE: Option<Locale> = Some(Locale::EnglishSimplified);
 
-    const TITLE: &'static str = "LLMs are not even dumb";
+    const TITLE: &'static str = "LLMs are not even stupid";
 
     const TAGS: &'static [Tag] = &[Tag::Ai, Tag::Tech];
 }
@@ -29,14 +30,46 @@ impl LazyRoute for NotEvenDumb {
     }
 
     fn view(_this: Self) -> AnyView {
+        let ai = || {
+            view! {
+                <Abbr no_expand=true title="Artificial Intelligence">
+                    "AI"
+                </Abbr>
+            }
+            .into_inner()
+        };
+        let llms = || {
+            view! {
+                <Abbr title="Large Language Model" suffix="s">
+                    "LLM"
+                </Abbr>
+            }
+            .into_inner()
+        };
         view! {
+            <a href="https://www.youtube.com/@FatherPhi">"FatherPhi"</a>
+            " is a great YouTube channel collecting examples of weird interactions with "
+            {ai}
+            ". I've cherry-picked a collection of videos that I feel demonstrate how "
+            {llms}
+            " aren't stupid, they're something else entirely."
+            <h2>"General intelligence"</h2>
             <div class:videos class:carousel-or-grid>
                 <YouTube video=youtube!("3fYiLXVfPa4" (9:16)) />
                 <YouTube video=youtube!("gPthZLTnzu8" (9:16)) />
                 <YouTube video=youtube!("bsl46vGpMNU" (9:16)) />
+                <YouTube video=youtube!("fcp1m-A-QwM" (9:16)) />
+            </div>
+            <h2>"Spelling issues"</h2>
+            "These are somewhat dismissable as the model using tokens instead of words."
+            <div class:videos class:carousel-or-grid>
                 <YouTube video=youtube!("7lRbNbwuczQ" (9:16)) />
                 <YouTube video=youtube!("oltt1pIYtyY" (9:16)) />
-                <YouTube video=youtube!("fcp1m-A-QwM" (9:16)) />
+                <YouTube video=youtube!("m_shkDLGWEQ" (9:16)) />
+            </div>
+            <h2>"Vision issues"</h2>
+            <div class:videos class:carousel-or-grid>
+                <YouTube video=youtube!("pBLvzATPxv8" (9:16)) />
             </div>
         }
         .into_any()
