36e20eb2c1e9b5cc02432fb4cf36c94e8c3466ad
Marcus <>
2026-07-24 09:11:31 +0200

    feat: allow contact card in env

--- qr-macro/src/lib.rs
+++ qr-macro/src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro::TokenStream;
 use qr::LogoPixel;
 use quote::{ToTokens, quote};
 use syn::{
-    Ident, LitStr, Token, bracketed,
+    Ident, LitStr, Token, bracketed, parenthesized,
     parse::{Parse, ParseStream},
     parse_macro_input,
     punctuated::Punctuated,
@@ -14,6 +14,8 @@ mod generator;
 
 use generator::generate_qr_code_with_logo;
 
+use crate::generator::QrGeneratorError;
+
 enum QrLogo {
     HasLogo {
         _plus_token: token::Plus,
@@ -23,11 +25,27 @@ enum QrLogo {
     None,
 }
 
+struct QrEnvFallback {
+    _or: Token![||],
+    val: LitStr,
+}
+
+enum QrContent {
+    Env {
+        _env: Ident,
+        _excl: Token![!],
+        _paren_token: token::Paren,
+        var: LitStr,
+        fallback: Option<QrEnvFallback>,
+    },
+    Raw(LitStr),
+}
+
 struct QrInput {
     const_token: Token![const],
     ident: Ident,
     _equals_token: token::Eq,
-    input: LitStr,
+    input: QrContent,
     logo: QrLogo,
     end_token: Option<token::Semi>,
 }
@@ -47,6 +65,46 @@ impl Parse for QrLogo {
     }
 }
 
+impl Parse for QrEnvFallback {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(QrEnvFallback {
+            _or: input.parse()?,
+            val: input.parse()?,
+        })
+    }
+}
+
+impl Parse for QrContent {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.peek(syn::Ident) {
+            let env: Ident = input.parse()?;
+            if env != "env" {
+                return Err(syn::Error::new(
+                    env.span(),
+                    format!("Invalid identifier {env}"),
+                ));
+            }
+            let var;
+            let mut result = QrContent::Env {
+                _env: env,
+                _excl: input.parse()?,
+                _paren_token: parenthesized!(var in input),
+                var: var.parse()?,
+                fallback: None,
+            };
+            if input.fork().parse::<QrEnvFallback>().is_ok() {
+                let QrContent::Env { fallback, .. } = &mut result else {
+                    unreachable!()
+                };
+                *fallback = Some(input.parse()?);
+            }
+            Ok(result)
+        } else {
+            Ok(QrContent::Raw(input.parse()?))
+        }
+    }
+}
+
 impl Parse for QrInput {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         Ok(QrInput {
@@ -58,6 +116,16 @@ impl Parse for QrInput {
             end_token: input.parse()?,
         })
     }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum QrMacroError {
+    #[error(transparent)]
+    Logo(#[from] LogoParseError),
+    #[error(transparent)]
+    Env(#[from] std::env::VarError),
+    #[error(transparent)]
+    Qr(#[from] QrGeneratorError),
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -90,6 +158,12 @@ fn print_error(d: impl std::fmt::Display) -> TokenStream {
 
 #[proc_macro]
 pub fn make_qr(input: TokenStream) -> TokenStream {
+    match make_qr_inner(parse_macro_input!(input as QrInput)) {
+        Ok(output) => output,
+        Err(e) => print_error(e),
+    }
+}
+fn make_qr_inner(input: QrInput) -> Result<TokenStream, QrMacroError> {
     let QrInput {
         const_token,
         ident,
@@ -97,62 +171,62 @@ pub fn make_qr(input: TokenStream) -> TokenStream {
         input,
         logo,
         end_token,
-    } = parse_macro_input!(input as QrInput);
+    } = input;
     let logo = match logo {
-        QrLogo::HasLogo { logo, .. } => into_logo(logo.into_iter()),
-        QrLogo::None => Ok(vec![]),
+        QrLogo::HasLogo { logo, .. } => into_logo(logo.into_iter())?,
+        QrLogo::None => vec![],
     };
-    match logo {
-        Ok(logo) => {
-            match generate_qr_code_with_logo(
-                &input.value(),
-                logo.into_iter().map(|x| x.into_iter()),
-                None,
-            ) {
-                Err(err) => print_error(err),
-                Ok(res) => {
-                    let width = res.width;
-                    let height = res.code.len() / width;
-                    let [logo_offset_x, logo_offset_y] = res.logo_offset;
-                    let inverts = res.logo_inverts;
-                    let i_width = res.logo_width;
-                    let i_height = inverts.len().checked_div(i_width).unwrap_or(0);
-                    let logo_data = {
-                        let mut logo_data = Default::default();
-                        res.code.chunks(width).for_each(|c| {
-                            let mut s = Default::default();
-                            c.iter().for_each(|x| quote! { #x, }.to_tokens(&mut s));
-                            quote! { [ #s ], }.to_tokens(&mut logo_data);
-                        });
-                        logo_data
-                    };
-                    let inv_data = {
-                        let mut inv_data = Default::default();
-                        if i_width > 0 {
-                            inverts.chunks(i_width).for_each(|c| {
-                                let mut s = Default::default();
-                                c.iter().for_each(|x| quote! { #x, }.to_tokens(&mut s));
-                                quote! { [ #s ], }.to_tokens(&mut inv_data);
-                            });
-                        }
-                        inv_data
-                    };
-
-                    let qr_type = quote! { qr::QrCodeWithLogo };
-                    let res = quote! {
-                        #const_token #ident: #qr_type<#width, #height, #logo_offset_x, #logo_offset_y, #i_width, #i_height> = #qr_type {
-                            code: [
-                                #logo_data
-                            ],
-                            inverted: [
-                                #inv_data
-                            ],
-                        } #end_token
-                    };
-                    res.into()
-                }
-            }
+    let input = match input {
+        QrContent::Env {
+            var,
+            fallback: None,
+            ..
+        } => std::env::var(var.value())?,
+        QrContent::Env {
+            var,
+            fallback: Some(fallback),
+            ..
+        } => std::env::var(var.value()).unwrap_or(fallback.val.value()),
+        QrContent::Raw(lit_str) => lit_str.value(),
+    };
+    let res = generate_qr_code_with_logo(&input, logo.into_iter().map(|x| x.into_iter()), None)?;
+    let width = res.width;
+    let height = res.code.len() / width;
+    let [logo_offset_x, logo_offset_y] = res.logo_offset;
+    let inverts = res.logo_inverts;
+    let i_width = res.logo_width;
+    let i_height = inverts.len().checked_div(i_width).unwrap_or(0);
+    let logo_data = {
+        let mut logo_data = Default::default();
+        res.code.chunks(width).for_each(|c| {
+            let mut s = Default::default();
+            c.iter().for_each(|x| quote! { #x, }.to_tokens(&mut s));
+            quote! { [ #s ], }.to_tokens(&mut logo_data);
+        });
+        logo_data
+    };
+    let inv_data = {
+        let mut inv_data = Default::default();
+        if i_width > 0 {
+            inverts.chunks(i_width).for_each(|c| {
+                let mut s = Default::default();
+                c.iter().for_each(|x| quote! { #x, }.to_tokens(&mut s));
+                quote! { [ #s ], }.to_tokens(&mut inv_data);
+            });
         }
-        Err(err) => print_error(err),
-    }
+        inv_data
+    };
+
+    let qr_type = quote! { qr::QrCodeWithLogo };
+    let res = quote! {
+        #const_token #ident: #qr_type<#width, #height, #logo_offset_x, #logo_offset_y, #i_width, #i_height> = #qr_type {
+            code: [
+                #logo_data
+            ],
+            inverted: [
+                #inv_data
+            ],
+        } #end_token
+    };
+    Ok(res.into())
 }
