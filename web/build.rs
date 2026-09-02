4304ea31f7490c1741efd4b207fa75f045624b47
Marcus <>
2026-09-02 10:13:14 +0200

    fix: only apply linker argument to wasm output

--- web/build.rs
+++ web/build.rs
@@ -1,3 +1,9 @@
+use std::env::var;
+
 fn main() {
-    println!("cargo:rustc-link-arg-cdylib=--no-merge-data-segments");
+    if let Ok(arch) = var("CARGO_CFG_TARGET_ARCH")
+        && arch.strip_prefix("wasm").is_some()
+    {
+        println!("cargo:rustc-link-arg-cdylib=--no-merge-data-segments");
+    }
 }
